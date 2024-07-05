9a4f1199c02b85cec2e1a11a4ed5b71083b6c8c3
vinson wong <vinson.wong@carbonwallet.com.hk>
2024-07-05 18:25:38 +0800

    new config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,6 +9,16 @@ return {
 
   -- These are some examples, uncomment them if you want to see them work!
   {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function()
+      local leap = require('leap')
+      leap.add_default_mappings()
+      leap.opts.case_sensitive = true
+    end,
+
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require("nvchad.configs.lspconfig").defaults()
@@ -28,35 +38,35 @@ return {
   },
   {
     "mfussenegger/nvim-dap",
-	dependencies = {
-		"leoluz/nvim-dap-go",
-		"rcarriga/nvim-dap-ui",
-	},
-   lazy=false,
-	config = function()
-    require("dapui").setup()
-		require("dap-go").setup()
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+    },
+    lazy = false,
+    config = function()
+      require("dapui").setup()
+      require("dap-go").setup()
 
-		local dap, dapui = require("dap"), require("dapui")
+      local dap, dapui = require("dap"), require("dapui")
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-		-- vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
-		-- vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
-		-- vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-		-- vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
-	end,
+      -- vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
+      -- vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+      -- vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+      -- vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+    end,
   },
   {
     "williamboman/mason.nvim",
